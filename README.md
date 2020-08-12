6122e3ba484df70d32a2493ff0505b76e59e510b
Gogs <r14r@via-internet.de>
2020-08-12 09:27:42 +0200

    -

---
+++ README.md
@@ -0,0 +1,20 @@
+# Behavior-Driven Development with Django and Aloe
+
+## Want to learn how to build this project?
+
+Check out the [blog post](https://testdriven.io/behavior-driven-development-with-django-and-aloe).
+
+## Want to use this project?
+
+1. Fork/Clone
+
+1. Create and activate a virtualenv
+
+1. Install the requirements
+
+1. Run the tests:
+
+    ```
+    $ cd example_bdd
+    $ python manage.py harvest
+    ```
---
+++ example_bdd/example/features/friendships_steps.py
@@ -0,0 +1,83 @@
+from aloe import before, step, world
+from aloe.tools import guess_types
+from aloe_django.steps.models import get_model, reset_sequence
+from nose.tools import assert_count_equal, assert_dict_equal, assert_true
+
+from django.contrib.auth.models import User
+
+from rest_framework.test import APIClient
+
+from ..models import Friendship
+
+
+@before.each_feature
+def before_each_feature(feature):
+    world.client = APIClient()
+
+
+@step('I create the following users:')
+def step_create_users(self):
+    for user in guess_types(self.hashes):
+        User.objects.create_user(**user)
+
+
+@step('I log in with username "([^"]+)" and password "([^"]+)"')
+def step_log_in(self, username, password):
+    world.is_logged_in = world.client.login(username=username, password=password)
+
+
+@step('I am logged in')
+def step_confirm_log_in(self):
+    assert_true(world.is_logged_in)
+
+
+@step('I empty the "([^"]+)" table')
+def step_empty_table(self, model_name):
+    model_class = get_model(model_name)
+    model_class.objects.all().delete()
+    reset_sequence(model_class)
+
+
+@step('I get a list of friends')
+def step_get_friends(self):
+    world.response = world.client.get('/friends/')
+
+
+@step('I see the following response data:')
+def step_confirm_response_data(self):
+    response = world.response.json()
+    if isinstance(response, list):
+        assert_count_equal(guess_types(self.hashes), response)
+    else:
+        assert_dict_equal(guess_types(self.hashes)[0], response)
+
+
+@step('I create the following friendships:')
+def step_create_friendships(self):
+    Friendship.objects.bulk_create([
+        Friendship(
+            id=data['id'],
+            user1=User.objects.get(id=data['user1']),
+            user2=User.objects.get(id=data['user2']),
+            status=data['status']
+        ) for data in guess_types(self.hashes)
+    ])
+
+
+@step('I request the following friendship:')
+def step_request_friendship(self):
+    world.response = world.client.post('/friendship-requests/', data=guess_types(self.hashes[0]))
+
+
+@step('I accept the friendship request with ID "([^"]+)"')
+def step_accept_friendship_request(self, pk):
+    world.response = world.client.put(f'/friendship-requests/{pk}/', data={
+      'status': Friendship.ACCEPTED
+    })
+
+
+@step('I reject the friendship request with ID "([^"]+)"')
+def step_reject_friendship_request(self, pk):
+    world.response = world.client.put(f'/friendship-requests/{pk}/', data={
+      'status': Friendship.REJECTED
+    })
